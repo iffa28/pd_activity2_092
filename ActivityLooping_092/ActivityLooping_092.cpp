ea98bcb9a10bc8c05b2b1ab5a4a6340a99ccd0d7
iffa28 <127277497+iffa28@users.noreply.github.com>
2023-03-22 17:01:41 +0700

    menjalankan program

--- ActivityLooping_092/ActivityLooping_092.cpp
+++ ActivityLooping_092/ActivityLooping_092.cpp
@@ -13,7 +13,20 @@ float luaslingkaran(float r) {
 	return 3.14 * r * r;
 }
 
-
 int main() {
-	float panjang, lebar, 
+	float panjang, lebar, tinggi, jejari, alas;
+	int pilihan;
+
+	cout << "===================" << endl;
+	cout << "======M E N U======" << endl;
+	cout << "===================" << endl;
+
+	cout << "1. luas persegi panjang" << endl;
+	cout << "2. luas segitiga" << endl;
+	cout << "3. luas lingkaran" << endl;
+	cout << "exit" << endl;
+
+	cout << "masukkan pilihan" << endl;
+	cout << "\npilihan (1/2/3/4) : " ;
+	cin >> pilihan;
 }
