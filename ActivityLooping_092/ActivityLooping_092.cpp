e9d5ee7a2ffe2ad5db8f5813a4149ef4050ebb42
iffa28 <127277497+iffa28@users.noreply.github.com>
2023-03-22 16:23:43 +0700

    Add project files.

---
+++ ActivityLooping_092/ActivityLooping_092.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+float luaspersegipanjang(float p, float l) {
+	return p * l;
+}
+
+float luassegitiga(float a, float t) {
+	return 0.5 * a * t;
+}
+
+float Luaslingkaran(float r) {
+	return 3.14 * r * r;
+}
+
+
+int main() {
+	float panjang, lebar, 
+}
